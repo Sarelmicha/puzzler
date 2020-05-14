be4c0dd49d374f843aadad7d14534b3729ef5d68
Sarel <sarelmicha93@gmail.com>
2020-05-14 14:31:50 +0300

    *Firestorage has been added and save image has been set

--- lib/repository/game_repository.dart
+++ lib/repository/game_repository.dart
@@ -1,32 +1,59 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:puzzlechat/data/user.dart';
 import 'package:puzzlechat/repository/user_repository.dart';
 import 'package:puzzlechat/util/converter.dart';
 
 class GameRepository {
-
+  FirebaseStorage _fireStorage;
   Firestore _firestore;
   Converter converter;
   UserRepository userRepository;
 
   GameRepository() {
+    this._fireStorage = FirebaseStorage.instance;
     this._firestore = Firestore.instance;
     userRepository = UserRepository();
     this.converter = Converter();
   }
 
+  void sendGame(String receiverPhoneNumber, File imageFile, int totalTime,
+      int numOfPieces, String senderPhoneNumber) async {
+    var document = await _firestore
+        .collection('users')
+        .document(receiverPhoneNumber)
+        .get();
 
-  void sendGame(String receiverPhoneNumber,List<int> image,int totalTime, int numOfPieces,String senderPhoneNumber) async{
+    User user = User(
+        phoneNumber: document.data['phoneNumber'],
+        newGames: document.data['newGames'],
+        active: true);
 
-    var document = await  _firestore.collection('users').document(receiverPhoneNumber).get();
+    String downloadImageUrl =
+        await saveImageFileToStorage(imageFile, senderPhoneNumber);
 
-    User user = User(phoneNumber: document.data['phoneNumber'],newGames: document.data['newGames'], active: true);
-
-    user.newGames[senderPhoneNumber] = image;
+    user.newGames[senderPhoneNumber] = downloadImageUrl;
 
     userRepository.saveUser(user);
-
   }
 
+  Future<String> saveImageFileToStorage(
+      File imageFile, String senderPhoneNumber) async {
+    //TODO - in futrue need to change the child id
+
+    //Create a reference to the location you want to upload to in firebase
+    StorageReference reference = _fireStorage.ref().child(senderPhoneNumber);
+
+    //Upload the file to firebase
+    StorageUploadTask uploadTask = reference.putFile(imageFile);
+
+    StorageTaskSnapshot taskSnapshot = await uploadTask.onComplete;
+
+    // Waits till the file is uploaded then stores the download url
+    String url = await taskSnapshot.ref.getDownloadURL();
+
+    return url;
+  }
 }
